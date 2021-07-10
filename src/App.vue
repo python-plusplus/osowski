ebf12e92dc98480d018c53e99a415b5326543869
python-plusplus <sunkim92941@nbss.nbed.nb.ca>
2021-07-09 22:04:53 -0300

    v.1

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template> 
+  <HelloWorld msg="Welcome to the gucci gang yeet"/>
+  <TestButton/>
+  <TestButton/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import TestButton from './components/TestButton.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    TestButton
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TestButton.vue
@@ -0,0 +1,19 @@
+<template>
+
+<button @click="count++"> You clicked on this button {{ count }} times! </button> 
+</template>
+
+<script>
+export default {
+    name: 'TestButton',
+    data: function() {
+        return { 
+            count: 0
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
