6f317874965674de0a7e803c95ae5b26eafa71b7
python-plusplus <sunkim92941@nbss.nbed.nb.ca>
2021-07-13 09:55:17 -0300

    router added

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,9 @@
 <template> 
+  <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+  </div>
+  <router-view />
   <HelloWorld msg="Welcome to the gucci gang yeet"/>
   <TestButton/>
   <TestButton/>
---
+++ src/views/About.vue
@@ -0,0 +1,10 @@
+<template>
+ <h1>Hello aAbout</h1>   
+</template>
+
+<script>
+export default{
+    name: 'About'
+}
+</script>
+
