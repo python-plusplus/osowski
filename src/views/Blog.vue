69fce35b02d6ea8c9ae4ed0f0a2184c3da311a11
python-plusplus <sunkim92941@nbss.nbed.nb.ca>
2021-08-05 10:31:18 -0300

    adding basic BlogCard

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,9 +1,20 @@
 <template>
- <h1>Blog</h1>   
+<div>
+    <BlogCard v-for="(post, index) in SampleBlogCards" :key="index" />
+    <h1>Blog</h1>   
+</div>    
 </template>
 
 <script>
+import BlogCard from '../components/BlogCard.vue'; 
+
 export default{
-    name: 'Blog'
+    name: 'Blog',
+    components: { BlogCard },
+    data() {
+        return{
+            sampleBlogCards: []
+        } 
+    }
 }
 </script>
